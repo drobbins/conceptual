364f5d3b5b39d5927035152d9d8ff812623263d2
David Robbins <robbins.david@gmail.com>
2011-12-06 16:50:36 -0600

    Initial backbone model for concepts and maps

--- _attachments/scripts/loader.js
+++ _attachments/scripts/loader.js
@@ -1,13 +1,16 @@
 (function(){
-    function app_load(scripts) {
-      for (var i=0; i < scripts.length; i++) {
-        document.write('<script src="'+scripts[i]+'"><\/script>')
-      };
+  function app_load(scripts) {
+    for (var i=0; i < scripts.length; i++) {
+    document.write('<script src="'+scripts[i]+'"><\/script>')
     };
+  };
 
-    app_load([
-        "scripts/jquery.min.js",
-        "scripts/underscore-min.js",
-        "scripts/backbone.js"
-    ]);
+  app_load([
+    "scripts/d3.min.js",
+    "scripts/d3.layout.min.js",
+    "scripts/d3.geom.min.js",
+    "scripts/jquery.min.js",
+    "scripts/underscore-min.js",
+    "scripts/backbone.js"
+  ]);
 })();
